88b65c03a07fa4ac7504905a45a593feb2a301fe
System Administrator <root@Tjeerds-MBP.lan>
2017-01-22 17:38:17 +0100

    second comit

---
+++ src/ProjectOne.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Created by tjeerd on 1/22/17.
+ */
+public class ProjectOne extends JFrame  {
+
+    public static JLabel label;
+    public static ImageIcon img, img2, img3, img4 ,img5, img6;
+
+    public static void main(String[] args) {
+        ProjectOne frame = new ProjectOne();
+
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+
+        JLabel label = new JLabel("test");
+        frame.add(label);
+        frame.setTitle("test");
+
+    }
+
+    public ProjectOne() {
+        img = new ImageIcon(getClass().getResource("dobbel1.jpg"));
+        img2 = new ImageIcon(getClass().getResource("dobbel2.jpg"));
+        img3 = new ImageIcon(getClass().getResource("dobbel3.jpg"));
+        img4 = new ImageIcon(getClass().getResource("dobbel4.jpg"));
+        img5 = new ImageIcon(getClass().getResource("dobbel5.jpg"));
+        img6 = new ImageIcon(getClass().getResource("dobbel6.jpg"));
+        setLayout(new FlowLayout());
+        label = new JLabel(img);
+        add(label);
+        JButton but1 = new JButton("dice");
+        add(but1);
+        Event e = new Event();
+        but1.addActionListener(e);
+        JTextField text = new JTextField("hoi");
+        add(text);
+
+    }
+
+
+}
+
+
